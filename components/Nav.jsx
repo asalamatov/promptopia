d0ae0957b9c23dd8979af09c9e3cb6073670ce1c
Azamat Salamatov <azamat.swift@icloud.com>
2023-10-02 07:20:09 -0500

    updated page

---
+++ components/Nav.jsx
@@ -0,0 +1,146 @@
+'use client';
+
+import Link from "next/link"; // <a href></a>
+/*Next.js's next/link component is a drop-in replacement for HTML anchor tags. It's recommended over the native anchor tag because it has built-in features that improve application performance and SEO ranking. Here are some differences between next/link and a tags: next/link doesn't trigger a full page reload when a user clicks on the link. Instead, it uses client-side navigation to load the new page while preserving the current state of the application. a tags create a standard hyperlink which directs the user to the URL as a new page. Link is for links that need no highlighting. a is for external links.  next/link is more accessible because you can cover it to see where it leads or right click and open it in a new tab.*/
+
+import Image from "next/image";
+/* next/image is an extension of the HTML img element, evolved for the modern web. This means that resizing, optimizing, and serving images in modern formats like WebP (when the browser supports it) can be done automatically using next/image . */
+
+import { useState, useEffect } from 'react';
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
+
+
+
+const Nav = () => {
+  const isUserLoggedIn = true;
+
+  const [providers, setProviders] = useState(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const res = await getProviders();
+
+      setProviders(res);
+    };
+
+    setProviders();
+  }, [])
+
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          width={30}
+          height={30}
+          alt="Promptopia Logo"
+          className=""
+        />
+        <p className="max-sm:hidden font-satoshi font-semibold text-lg text-black tracking-wide">
+          Promptopia
+        </p>
+      </Link>
+
+      {/* Desktop Navigation */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+
+            <button
+              className="outline_btn"
+              onClick={signOut}
+              type="button"
+            >
+              Sign Out
+            </button>
+
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                width={37}
+                height={37}
+                alt="Profile Image"
+                className="rounded-full"
+              />
+            </Link>
+          </div>
+        ) : (
+          <div>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </div>
+        )}
+      </div>
+
+      {/* Mobile Navigation */}
+      <div className="sm:hidden flex relative">
+        {isUserLoggedIn ? (
+          <>
+            <Image
+              src="/assets/images/logo.svg"
+              width={37}
+              height={37}
+              alt="Profile Image"
+              className="rounded-full"
+              onClick={() => setToggleDropdown(!toggleDropdown)}
+            />
+
+            {toggleDropdown && (
+              <div className="dropdown">
+                <Link
+                  href="/profile"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href="/create-prompt"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button className="mt-5 w-full black_btn" type="button" onClick={() => {
+                  setToggleDropdown(false);
+                  signOut();
+                }}>
+                  Sign Out
+                </button>
+              </div>
+            )}
+          </>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+    </nav>
+  );
+}
+
+export default Nav
