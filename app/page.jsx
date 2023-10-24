7f87dbb13112eaebd77a9e9d4257f4eb186a270d
Azamat Salamatov <azamat.swift@icloud.com>
2023-10-24 08:56:18 -0500

    added auth

--- app/page.jsx
+++ app/page.jsx
@@ -7,11 +7,11 @@ const Home = () => {
       <h1 className="head_text text-center">
         Discover & Share
         <br className="max-md:hidden"/>
-        <span className="orange_gradient"> AI-Powered Prompts</span>
+        <span className="orange_gradient"> Azamat's Prompts</span>
       </h1>
       <p className="desc text-center">
         Promptopia is an open-source AI prompting tool for modern
-        world t discover, create and share creative prompts
+        world to discover, create and share creative prompts
       </p>
 
       <Feed />
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -12,19 +12,20 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  // const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const res = await getProviders();
 
       setProviders(res);
     };
 
-    setProviders();
+    setUpProviders();
   }, [])
 
   return (
@@ -44,7 +45,7 @@ const Nav = () => {
 
       {/* Desktop Navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -60,7 +61,7 @@ const Nav = () => {
 
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 alt="Profile Image"
@@ -87,10 +88,10 @@ const Nav = () => {
 
       {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
-          <>
+        {session?.user ? (
+          <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
               alt="Profile Image"
@@ -122,7 +123,7 @@ const Nav = () => {
                 </button>
               </div>
             )}
-          </>
+          </div>
         ) : (
           <>
             {providers &&
